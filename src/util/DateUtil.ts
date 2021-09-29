0c598c10cbc78b8f3c61c841c7b05c7253d2ce0f
Chris-Bitler <chris@cbitler.io>
2021-09-28 22:59:04 -0700

    Fix timezone stuff

--- src/util/DateUtil.ts
+++ src/util/DateUtil.ts
@@ -2,28 +2,12 @@
  * Get chrono custom in EST
  */
 import * as chrono from 'chrono-node';
-import { ParsingResult } from 'chrono-node/dist/results';
-
-export const getChronoCustom = () => {
-    const custom = new chrono.Chrono();
-    custom.refiners.push({
-        refine: (context, results): ParsingResult[] => {
-            results.forEach((result) => {
-                result.start.imply('timezoneOffset', -300);
-                result.end && result.end.imply('timezoneOffset', -300);
-            });
-            return results;
-        }
-    });
-
-    return custom;
-};
 
 export const parseDateTimeInput = (str: string) => {
-    const chrono = getChronoCustom();
-    let parsedDate = chrono.parseDate(str);
+    const chronoInstance = new chrono.Chrono();
+    let parsedDate = chronoInstance.parseDate(str);
     if (!parsedDate) {
-        parsedDate = chrono.parseDate(`${str} from now`);
+        parsedDate = chronoInstance.parseDate(`${str} from now`);
     }
 
     return parsedDate;
